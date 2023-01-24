ab9caedcdc9a29e1ce1d742d8fed3486eacc79d8
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2023-01-24 09:50:37 -0500

    created color palette w hex codes

---
+++ color_palette.R
@@ -0,0 +1,19 @@
+woodland_palette = c('#231f20',
+                      '#1c3c4a',
+                      '#344a2f',
+                      '#886ea9'
+                      '#e02c2d',
+                      '#ffb400',
+                      '#d2e6ee',
+                      '#efefef',
+                      '#e4eee6',
+                      '#e8e8e8',
+                      '#ecdace',
+                      '#fff3d6',
+                      '#9bc6d9',
+                      '#c2c2c2',
+                      '#a9aa7e',
+                      '#d5cce1',
+                      '#c79191',
+                      '#ffe099'
+                    )
