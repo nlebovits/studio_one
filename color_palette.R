2e74fa86f8e01ced93039aa220dadd280983ca3d
Nissim Lebovits <111617674+nlebovits@users.noreply.github.com>
2023-01-24 10:01:35 -0500

    added dark and light pals

--- color_palette.R
+++ color_palette.R
@@ -1,4 +1,4 @@
-woodland_palette = c('#231f20',
+woodland_palette_full = c('#231f20',
                       '#1c3c4a',
                       '#344a2f',
                       '#886ea9',
@@ -17,3 +17,27 @@ woodland_palette = c('#231f20',
                       '#c79191',
                       '#ffe099'
                     )
+
+# define dark color palette
+woodland_palette_dark = c('#231f20',
+                                   '#1c3c4a',
+                                   '#344a2f',
+                                   '#886ea9',
+                                   '#e02c2d',
+                                   '#ffb400',
+)
+
+# define light color palette
+woodland_palette_light = c('#d2e6ee',
+                                    '#efefef',
+                                    '#e4eee6',
+                                    '#e8e8e8',
+                                    '#ecdace',
+                                    '#fff3d6',
+                                    '#9bc6d9',
+                                    '#c2c2c2',
+                                    '#a9aa7e',
+                                    '#d5cce1',
+                                    '#c79191',
+                                    '#ffe099'
+)
